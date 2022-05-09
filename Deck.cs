f8ec25d492c87712e3d80b1b901d7ebfcd260012
toby3adams <toby3adams@byui.edu>
2022-05-09 11:34:12 -0600

    defined the cardsArray and made an initializing function for said array to call before the game loop in director

--- Deck.cs
+++ Deck.cs
@@ -2,7 +2,14 @@ namespace hi_low_game
 {
     class Deck{
 
-    
+        int[] cardsArray = new int[13];
+
+
+        // Defines the initial set of cards in the deck - Sam
+        public void initialCardsArray(){
+            cardsArray = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+        }
+
 
 
 
