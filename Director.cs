e1421a3f21a1bc0c301f3e402ae9a866220c8e57
thussaiththelaw <93949213+thussaiththelaw@users.noreply.github.com>
2022-05-09 11:17:25 -0600

    Update Director.cs

--- Director.cs
+++ Director.cs
@@ -2,6 +2,20 @@ namespace hi_low_game
 {
     class Director
     {
-        
+        public void StartGame()
+        {
+            bool keepPlaying = true;
+            do
+            {
+                GetInputs();
+                DoUpdates();
+                DoOutputs();
+            } while (keepPlaying);
+        }
+//get inputs from the user
+        public void GetInputs()
+        {
+            return;
+        }
     }
 }
